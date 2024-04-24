eb3ec189d788f777a2b91763c7847e13908edf1f
Talha Malik <talhamalik25.tm@gmail.com>
2024-04-24 23:34:57 +0500

    Added range and reset button

--- src/App.js
+++ src/App.js
@@ -1,48 +1,60 @@
-import React, { useState } from 'react';
-import './app.css';
+import React, { useState } from "react";
+import "./app.css";
 
-const Counter = () =>
-{
-    const [ count, setCount ] = useState( 0 );
-    const [ step, setStep ] = useState( 1 );
+const Counter = () => {
+	const [count, setCount] = useState(0);
+	const [step, setStep] = useState(1);
 
-    const date = new Date();
-    date.setDate( date.getDate() + count );
+	const date = new Date();
+	date.setDate( date.getDate() + count );
+	
+	const handleReset = () =>
+	{
+		setCount( 0 );
+		setStep( 1 );
+	}
 
-    return (
+	return (
+		<div>
 			<div>
-				<div>
-					<button onClick={() => setStep((prev) => prev - 1)}>-</button>
-					<span>Step: {step}</span>
-					<button onClick={() => setStep((prev) => prev + 1)}>+</button>
-				</div>
-				<div>
-					<button onClick={() => setCount((prev) => prev - step)}>-</button>
-					<span>Count: {count}</span>
-					<button onClick={() => setCount((prev) => prev + step)}>+</button>
-				</div>
-				<p>
-					<span>
-						{count === 0
-							? "Today is "
-							: count > 0
-							? `${count} days from today is `
-							: `${-count} days ago was `}
-					</span>
-					<span>{date.toDateString()}</span>
-				</p>
+				<input
+					type="range"
+					value={step}
+					max={10}
+					onChange={(e) => setStep(Number(e.target.value))}
+				/>
+				<span>Step: {step}</span>
 			</div>
-		);
-    
-}
+			<div>
+				<button onClick={() => setCount((prev) => prev - step)}>-</button>
+				<input
+					type="number"
+					value={count}
+					onChange={(e) => setCount(e.target.value)}
+				/>
+				<button onClick={() => setCount((prev) => prev + step)}>+</button>
+			</div>
+			<p>
+				<span>
+					{count === 0
+						? "Today is "
+						: count > 0
+						? `${count} days from today is `
+						: `${-count} days ago was `}
+				</span>
+				<span>{date.toDateString()}</span>
+			</p>
+			{ (count === 0 && step === 1) || <button type="reset" onClick={ handleReset }>Reset</button> }
+		</div>
+	);
+};
 
-const App = () =>
-{
-    return (
-        <div className='App'>
-            <Counter />
-        </div>
-    )
+const App = () => {
+	return (
+		<div className="App">
+			<Counter />
+		</div>
+	);
 };
 
 export default App;
